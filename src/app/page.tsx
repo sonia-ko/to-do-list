7a11455d01701e5e8796d56f0469da7ec7282610
Sonya Kozytska <sonya-ko@outlook.com>
2024-03-06 12:50:44 +0100

    add media queries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
+import Head from "next/head";
 import styles from "./page.module.css";
 import ToDosContainer from "@/components/to-dos/to-dos-container/ToDosContainer";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <ToDosContainer />
-    </main>
+    <>
+      <Head>
+        <title>Simple To-do list</title>
+        <meta name="description" content="A simple to-do list" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <main className={styles.main}>
+        <ToDosContainer />
+      </main>
+    </>
   );
 }
